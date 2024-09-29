95ee7d2fc2c33c6e56f11bb1c7e8f7ce6900e8bf
wefcdse <3371348169@qq.com>
2024-09-29 11:48:23 +0800

    init

---
+++ src/client/java/com/iung/ccwasm/CcwasmClient.java
@@ -0,0 +1,10 @@
+package com.iung.ccwasm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CcwasmClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/iung/ccwasm/CcwasmDataGenerator.java
@@ -0,0 +1,11 @@
+package com.iung.ccwasm;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CcwasmDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
