ee535b511c6ddbef91e10c9cf00529e76d84d18c
wefcdse <3371348169@qq.com>
2025-11-10 18:30:11 +0800

    Signed-off-by: wefcdse <3371348169@qq.com>

---
+++ src/main/resources/data/computercraft/lua/rom/programs/exec_wasm_noaot.lua
@@ -0,0 +1,25 @@
+Args = { ... }
+wasm_name = Args[1]
+-- print(wasm_name)
+
+_G.args = {}
+for index, value in ipairs(Args) do
+    if index > 1 then
+        args[index - 1] = value
+    end
+end
+
+
+_G.global = _G
+_G.wasm_mod = wasm.load_wasm(wasm_name, false)
+wasm_mod.init()
+
+while true do
+    wasm_mod.tick()
+    if wasm_mod.eval_string() ~= nil then
+        wasm_mod.eval_result(load(wasm_mod.eval_string())())
+    end
+    if wasm_mod.stopped() then
+        break
+    end
+end
